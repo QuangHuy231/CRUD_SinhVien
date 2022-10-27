84c74e3eceb199fbb61734727e59f932687d5abd
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-10-27 11:02:09 +0700

    create Add page add sinhvien

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
-  return <div>Create</div>;
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    lop: "",
+    gioitinh: null,
+  });
+  const handleChange = (e) => {
+    setUser((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const navigate = useNavigate();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8800/create-user", user);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Add New Sinh Vien</h1>
+
+      <input
+        type="text"
+        placeholder="First Name"
+        name="firstName"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        name="lastName"
+        onChange={handleChange}
+      />
+      <input type="text" placeholder="Lop" name="lop" onChange={handleChange} />
+      <input
+        type="number"
+        placeholder="Gioi Tinh"
+        name="gioitinh"
+        onChange={handleChange}
+      />
+
+      <button className="formButton" onClick={handleClick}>
+        Add
+      </button>
+    </div>
+  );
 };
 
 export default Create;
